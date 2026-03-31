79dfcad9ffb1ca1c368751ce93bd46bda99bdaa2
Pedro Lucas <plfm3k@gmail.com>
2026-03-31 19:47:37 -0400

    feat: adiciona método para encontrar consulta com médico e hora repetida

--- src/main/java/dev/pdrolcs/gestao_consulta_medica_api/repositories/ConsultaRepository.java
+++ src/main/java/dev/pdrolcs/gestao_consulta_medica_api/repositories/ConsultaRepository.java
@@ -4,13 +4,16 @@ import dev.pdrolcs.gestao_consulta_medica_api.models.Consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 
-    List<Consulta> findByPacientId(Long id);
+    List<Consulta> findByPacienteId(Long paciente_id);
 
-    List<Consulta> findByMedicoId(Long id);
+    List<Consulta> findByMedicoId(Long medico_id);
+
+    boolean existsByMedicoIdAndDataHora(Long medico_id, LocalDateTime dataHora);
 
 }
