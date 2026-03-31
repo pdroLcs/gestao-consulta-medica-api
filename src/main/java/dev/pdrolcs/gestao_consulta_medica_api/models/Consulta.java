f4af90ba632373bea2df518a80ed6ed14e09c4ab
Pedro Lucas <plfm3k@gmail.com>
2026-03-31 19:44:37 -0400

    fix: remove final dos atributos

--- src/main/java/dev/pdrolcs/gestao_consulta_medica_api/models/Consulta.java
+++ src/main/java/dev/pdrolcs/gestao_consulta_medica_api/models/Consulta.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Table(name = "consultas")
 @Getter
 @Setter
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Consulta {
 
     @Id
@@ -18,17 +18,23 @@ public class Consulta {
 
     @ManyToOne
     @JoinColumn(name = "paciente_id", nullable = false)
-    private final Paciente paciente;
+    private Paciente paciente;
 
     @ManyToOne
     @JoinColumn(name = "medico_id", nullable = false)
-    private final Medico medico;
+    private Medico medico;
 
     @Column(nullable = false)
-    private final LocalDateTime dataHora;
+    private LocalDateTime dataHora;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private final StatusEnum status;
+    private StatusEnum status;
 
+    public Consulta(Paciente paciente, Medico medico, LocalDateTime dataHora, StatusEnum status) {
+        this.paciente = paciente;
+        this.medico = medico;
+        this.dataHora = dataHora;
+        this.status = status;
+    }
 }
