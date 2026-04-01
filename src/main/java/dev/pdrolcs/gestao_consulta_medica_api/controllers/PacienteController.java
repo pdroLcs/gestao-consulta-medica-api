0406b36db6d39b1ed87f009960e1b0e50c95d66d
Pedro Lucas <plfm3k@gmail.com>
2026-04-01 12:59:37 -0400

    feat: implementa controller PacienteController

--- src/main/java/dev/pdrolcs/gestao_consulta_medica_api/controllers/PacienteController.java
+++ src/main/java/dev/pdrolcs/gestao_consulta_medica_api/controllers/PacienteController.java
@@ -1,4 +1,40 @@
 package dev.pdrolcs.gestao_consulta_medica_api.controllers;
 
+import dev.pdrolcs.gestao_consulta_medica_api.dto.PacienteRequestDTO;
+import dev.pdrolcs.gestao_consulta_medica_api.dto.PacienteResponseDTO;
+import dev.pdrolcs.gestao_consulta_medica_api.services.PacienteService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pacientes")
 public class PacienteController {
+
+    private final PacienteService pacienteService;
+
+    public PacienteController(PacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PacienteResponseDTO>> listarPacientes() {
+        return ResponseEntity.ok(pacienteService.listarPacientes());
+    }
+
+    @PostMapping
+    public ResponseEntity<PacienteResponseDTO> cadastrarPaciente(@Valid @RequestBody PacienteRequestDTO pacienteRequest) {
+        var paciente = pacienteService.criarPaciente(pacienteRequest);
+        var uri = URI.create("/pacientes/" + paciente.id());
+        return ResponseEntity.created(uri).body(paciente);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteResponseDTO> buscarPorId(@RequestParam Long id) {
+        return ResponseEntity.ok(pacienteService.buscarPorId(id));
+    }
 }
