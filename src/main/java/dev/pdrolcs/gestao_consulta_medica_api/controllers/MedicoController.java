75eb8f4658049fa7a6d8e16ae813e212b2c91b5b
Pedro Lucas <plfm3k@gmail.com>
2026-04-06 19:21:44 -0400

    feat: implementa MedicoController

--- src/main/java/dev/pdrolcs/gestao_consulta_medica_api/controllers/MedicoController.java
+++ src/main/java/dev/pdrolcs/gestao_consulta_medica_api/controllers/MedicoController.java
@@ -1,12 +1,13 @@
 package dev.pdrolcs.gestao_consulta_medica_api.controllers;
 
+import dev.pdrolcs.gestao_consulta_medica_api.dto.MedicoRequestDTO;
 import dev.pdrolcs.gestao_consulta_medica_api.dto.MedicoResponseDTO;
 import dev.pdrolcs.gestao_consulta_medica_api.services.MedicoService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,5 +23,12 @@ public class MedicoController {
     @GetMapping
     public ResponseEntity<List<MedicoResponseDTO>> listarMedicos() {
         return ResponseEntity.ok(medicoService.listarMedicos());
+    }
+
+    @PostMapping
+    public ResponseEntity<MedicoResponseDTO> criarMedico(@Valid @RequestBody MedicoRequestDTO medicoRequest) {
+        var medico = medicoService.criarMedico(medicoRequest);
+        var uri = URI.create("/medico/" + medico.id());
+        return ResponseEntity.created(uri).body(medico);
     }
 }
