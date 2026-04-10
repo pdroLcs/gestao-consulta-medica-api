2d7a5dcab65e6ab4c1eae0b3782cb87b3af50e33
Pedro Lucas <plfm3k@gmail.com>
2026-04-10 18:25:47 -0400

    feat: adiciona documentação dos endpoints de PacienteController

--- src/main/java/dev/pdrolcs/gestao_consulta_medica_api/controllers/PacienteController.java
+++ src/main/java/dev/pdrolcs/gestao_consulta_medica_api/controllers/PacienteController.java
@@ -3,8 +3,9 @@ package dev.pdrolcs.gestao_consulta_medica_api.controllers;
 import dev.pdrolcs.gestao_consulta_medica_api.dto.PacienteRequestDTO;
 import dev.pdrolcs.gestao_consulta_medica_api.dto.PacienteResponseDTO;
 import dev.pdrolcs.gestao_consulta_medica_api.services.PacienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/pacientes")
+@Tag(name = "Pacientes", description = "Operações relacionadas a pacientes")
 public class PacienteController {
 
     private final PacienteService pacienteService;
@@ -22,18 +24,22 @@ public class PacienteController {
     }
 
     @GetMapping
+    @Operation(summary = "Lista todos os pacientes")
     public ResponseEntity<List<PacienteResponseDTO>> listarPacientes() {
         return ResponseEntity.ok(pacienteService.listarPacientes());
     }
 
     @PostMapping
+    @Operation(summary = "Cadastra um novo paciente")
     public ResponseEntity<PacienteResponseDTO> cadastrarPaciente(@Valid @RequestBody PacienteRequestDTO pacienteRequest) {
         var paciente = pacienteService.criarPaciente(pacienteRequest);
         var uri = URI.create("/pacientes/" + paciente.id());
         return ResponseEntity.created(uri).body(paciente);
     }
 
+
     @GetMapping("/{id}")
+    @Operation(summary = "Lista o paciente pelo id")
     public ResponseEntity<PacienteResponseDTO> buscarPorId(@RequestParam Long id) {
         return ResponseEntity.ok(pacienteService.buscarPorId(id));
     }
